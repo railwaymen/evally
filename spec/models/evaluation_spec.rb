97768f2fb48c5f44402a6fdbfe2cdb14d3d435b0
Szymon Chodzidlo <szymon.chodzidlo@railwaymen.org>
2018-10-27 22:17:50 +0200

    Add model validations test specs

--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -7,4 +7,7 @@ RSpec.describe Evaluation, type: :model do
   it { is_expected.to define_enum_for(:state) }
 
   it { is_expected.to accept_nested_attributes_for(:sections).allow_destroy(false) }
+
+  it { is_expected.to validate_inclusion_of(:state).in_array(Evaluation.states.keys) }
+
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Section, type: :model do
   it { is_expected.to define_enum_for(:group) }
 
   it { is_expected.to define_enum_for(:width) }
+
+  it { is_expected.to validate_presence_of(:name) }
+
+  it { is_expected.to validate_presence_of(:group) }
+
+  it { is_expected.to validate_inclusion_of(:group).in_array(Section.groups.keys) }
+
+  it { is_expected.to validate_presence_of(:width) }
+
+  it { is_expected.to validate_inclusion_of(:width).in_array(Section.widths.keys) }
+
+  it { is_expected.to validate_presence_of(:position) }
+
+  it { should validate_numericality_of(:position).only_integer }
 end
