d31b90845f461b46e41a2c6d3a3155c0184b1c8e
Szymon Chodzidlo <szymon.chodzidlo@railwaymen.org>
2018-11-26 22:51:43 +0100

    Clear Vuex store on loggin out

--- app/javascript/application/store/modules/auth_store.js
+++ app/javascript/application/store/modules/auth_store.js
@@ -5,14 +5,16 @@ import { Utils } from '@/lib/utils'
 import { Setting } from '@/models/setting'
 import { User } from '@/models/user'
 
+const initialState = () => ({
+  user: new User(),
+  setting: new Setting(),
+  status: ''
+})
+
 const AuthStore = {
   namespaced: true,
 
-  state: {
-    user: new User(),
-    setting: new Setting(),
-    status: ''
-  },
+  state: initialState(),
 
   getters: {
     user: state => state.user,
@@ -27,12 +29,6 @@ const AuthStore = {
       state.status = 'signedIn'
       return state
     },
-    clearSession(state) {
-      state.user = new User()
-      state.setting = new Setting()
-      state.status = 'signedOut'
-      return state
-    },
     progress(state, status) {
       state.status = status
       return state
@@ -40,6 +36,15 @@ const AuthStore = {
     updateSetting(state, data) {
       state.setting = new Setting(data)
       return state
+    },
+    resetState(state) {
+      state = Object.assign(state, initialState())
+      return state
+    },
+
+    clearStore(state) {
+      let modules = ['Activities', 'Auth', 'Employees', 'Evaluations', 'Sections', 'Templates']
+      modules.forEach(module => this.commit(`${module}Store/resetState`))
     }
   },
 
@@ -67,7 +72,7 @@ const AuthStore = {
         localStorage.clear()
 
         delete axios.defaults.headers.common['Authorization']
-        context.commit('clearSession')
+        context.commit('clearStore')
 
         resolve()
       })
@@ -112,7 +117,7 @@ const AuthStore = {
             localStorage.clear()
 
             delete axios.defaults.headers.common['Authorization']
-            context.commit('clearSession')
+            context.commit('clearStore')
 
             resolve()
           })
