b2986e7668c4a3b826f9a0c3a3e4e5d939181f3e
Szymon Chodzidlo <szymon.chodzidlo@railwaymen.org>
2019-02-15 00:04:19 +0100

    Optimize employees browser widget

--- app/javascript/application/components/overview/EmployeesBrowser.vue
+++ app/javascript/application/components/overview/EmployeesBrowser.vue
@@ -5,23 +5,22 @@
     <div class="box__body">
       <v-form ref="employeesBrowserForm" @submit.prevent="search" class="browser-form">
         <v-select
-          @input="selectSkill"
+          class="browser-form__skill"
+          @input="setGroup"
+          v-model="query.name"
           :items="skillItems"
           :rules="[vRequired]"
-          item-text="name"
-          chips
-          return-object
           label="Skill"
-          class="browser-form__skill"
+          chips
         >
           <template slot="selection" slot-scope="data">
             <v-chip :selected="data.selected">
-              {{ data.item.name }}
+              {{ data.item }}
             </v-chip>
           </template>
 
           <template slot="item" slot-scope="data">
-            {{ data.item.name }}
+            {{ data.item }}
           </template>
         </v-select>
 
@@ -57,23 +56,27 @@
         <v-data-table
           :headers="headers"
           :items="results.models"
+          :loading="isLoading"
+          :custom-sort="customSort"
         >
+          <v-progress-linear slot="progress" color="blue" indeterminate></v-progress-linear>
           <template slot="items" slot-scope="props">
             <td>{{ props.item.fullname() }}</td>
             <td class="text-xs-center">{{ props.item.position }}</td>
             <td class="text-xs-center">{{ employment(props.item.hired_at) }}</td>
             <td class="text-xs-center">
-              <v-rating v-if="isRating(props.item.skill.value)" length="3" :value="props.item.skill.value"></v-rating>
+              <v-rating v-if="isRating(props.item.skill.value)" length="3" :value="props.item.skill.value" readonly></v-rating>
               <span v-if="isBool(props.item.skill.value)">
                 {{ props.item.skill.value ? `Yes` : `No` }}
               </span>
             </td>
-            <td class="text-xs-center">
-              <v-icon
-                small
-              >
-                list_alt
-              </v-icon>
+            <td>
+              <v-tooltip bottom>
+                <v-btn @click="showProfile(props.item.id)" slot="activator" color="grey" flat icon>
+                  <v-icon>person</v-icon>
+                </v-btn>
+                <span>View profile</span>
+              </v-tooltip>
             </td>
           </template>
         </v-data-table>
@@ -89,19 +92,24 @@ export default {
   name: 'EmployeesBrowser',
   data() {
     return {
+      query: {
+        group: 'rating',
+        name: '',
+        comparator: 'eq',
+        value: 0
+      },
       headers: [
         { text: 'Employee name', align: 'left', value: 'name' },
         { text: 'Position', value: 'position', align: 'center' },
         { text: 'Experience', value: 'experience', align: 'center' },
         { text: 'Skill', value: 'skill', align: 'center' },
-        { text: 'Actions', value: 'name' }
+        { text: 'Actions', value: 'name', sortable: false }
       ]
     }
   },
   methods: {
     search() {
       if (this.$refs.employeesBrowserForm.validate()) {
-
         this.$store.dispatch('BrowserStore/search', { query: this.query })
           .catch(error => {
             this.flash({ error: 'Browser error' })
@@ -109,14 +117,22 @@ export default {
       }
     },
 
-    selectSkill(skill) {
-      if (skill.group === 'bool') {
+    setGroup() {
+      if (this.skills.bool.indexOf(this.query.name) > -1) {
+        this.query.group = 'bool'
         this.query.comparator = 'eq'
         this.query.value = 1
+      } else {
+        this.query.group = 'rating'
+        this.query.value = 0
       }
+    },
 
-      this.query.name = skill.name
-      this.query.group = skill.group
+    showProfile(id) {
+      this.$store.commit('EmployeesStore/pick', id)
+      this.$router.push({ name: 'employees_path' })
+
+      this.$vuetify.goTo(0)
     },
 
     employment(hired_at) {
@@ -145,30 +161,53 @@ export default {
 
     isRating(val) {
       return this.$_.isNumber(val)
+    },
+
+    customSort(items, index, isDescending) {
+      items.sort((a, b) => {
+        switch(index) {
+          case 'experience':
+            if (isDescending) {
+              return this.$moment(b.hired_at).format('YYYYMMDD') - this.$moment(a.hired_at).format('YYYYMMDD')
+            } else {
+              return this.$moment(a.hired_at).format('YYYYMMDD') - this.$moment(b.hired_at).format('YYYYMMDD')
+            }
+            break
+          case 'skill':
+            if (this.isBool(a.skill.value) || this.isBool(b.skill.value)) break
+            return isDescending ? a.skill.value - b.skill.value : b.skill.value - a.skill.value
+            break
+          case 'name':
+            return isDescending ? b.fullname().localeCompare(a.fullname()) : a.fullname().localeCompare(b.fullname())
+            break
+          case 'position':
+            return isDescending ? b.position.localeCompare(a.position) : a.position.localeCompare(b.position)
+            break
+        }
+      })
+
+      return items
     }
   },
   computed: {
     ...mapGetters({
       skills: 'BrowserStore/skills',
       results: 'BrowserStore/results',
-      query: 'BrowserStore/query'
+      status: 'BrowserStore/status'
     }),
 
     skillItems() {
-      let ratingsExist = this.skills.rating && this.skills.rating.length > 0
-      let boolsExist = this.skills.bool && this.skills.bool.length > 0
-
       let output = []
 
-      if (ratingsExist) {
+      if (this.skills.rating.length > 0) {
         output.push({ header: 'Rating' }, ...this.skills.rating)
       }
 
-      if (ratingsExist && boolsExist) {
+      if (this.skills.rating.length > 0 && this.skills.bool.length > 0) {
         output.push({ divider: true })
       }
 
-      if (boolsExist) {
+      if (this.skills.bool.length > 0) {
         output.push({ header: 'Yes / No' }, ...this.skills.bool)
       }
 
@@ -180,6 +219,8 @@ export default {
       .catch(error => {
         this.flash({ error: 'Skills cannot be loaded due to some error.' })
       })
+    
+    this.query = this.$store.getters['BrowserStore/query']
   },  
 }
 </script>
--- app/services/v1/employees_skills_list_provider.rb
+++ app/services/v1/employees_skills_list_provider.rb
@@ -2,13 +2,10 @@ module V1
   class EmployeesSkillsListProvider
 
     def self.call
-      rating_skills = Section.rating.pluck(:skills).flatten.collect{ |skill| { name: skill['name'], group: 'rating' } }.uniq
-      bool_skills = Section.bool.pluck(:skills).flatten.collect{ |skill| { name: skill['name'], group: 'bool' } }.uniq
+      rating_skills = Section.rating.pluck(:skills).flatten.collect{ |skill| skill['name'] }.uniq
+      bool_skills = Section.bool.pluck(:skills).flatten.collect{ |skill| skill['name'] }.uniq
 
-      {
-        rating: rating_skills,
-        bool: bool_skills
-      }
+      { rating: rating_skills, bool: bool_skills }.to_json
     end
   end
 end
