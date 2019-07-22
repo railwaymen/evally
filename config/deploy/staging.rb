3a75efe52532610951612033f11cab31290322b1
Szymon Chodzidlo <szymon.chodzidlo@railwaymen.org>
2019-07-22 11:54:14 +0200

    Update ruby version in deploy files

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :deploy_to, ENV['CAP_STAGING_DEPLOY_TO']
 server ENV['CAP_STAGING_SERVER'], user: ENV['CAP_STAGING_SSH_USER'], roles: %w{app db web}
-set :rvm_ruby_version, 'ruby-2.5.3'
+set :rvm_ruby_version, 'ruby-2.6.3'
 set :branch, :develop
