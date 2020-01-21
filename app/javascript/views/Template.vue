27ceccbac76c8aac077dcb5843e6196ebd46974f
Szymon Chodzidlo <szymon.chodzidlo@railwaymen.org>
2020-01-21 22:27:32 +0100

    Place template composer inside form

--- app/javascript/views/Template.vue
+++ app/javascript/views/Template.vue
@@ -1,47 +1,51 @@
 <template>
   <div class="box box--with-content template">
-    <v-layout row wrap>
-      <v-flex xs12>
-        <div class="template__name">
-          <v-text-field
-            :label="$t('templates.forms.template_name')"
-            v-model="template.name"
-            :disabled="!template.editable"
+    <v-form ref="templateForm">
+      <v-layout row wrap>
+        <v-flex xs12>
+          <div class="template__name">
+            <v-text-field
+              :label="$t('templates.forms.template_name')"
+              v-model="template.name"
+              :disabled="!template.editable"
+            />
+          </div>
+        </v-flex>
+
+        <v-flex xs12 lg6>
+          <div class="employee">
+            <h3 class="employee__fullname">
+              &lsaquo; {{ $t('templates.placeholders.name') }} &rsaquo;
+            </h3>
+            <h4 class="employee__position">
+              &lsaquo; {{ $t('templates.placeholders.position') }} &rsaquo;
+            </h4>
+          </div>
+        </v-flex>
+
+        <v-flex xs6 lg3>
+          <div class="date">
+            <h5 class="date__value">&lsaquo; date &rsaquo;</h5>
+            <h6 class="date__description">{{ $t('evaluations.forms.hired_at') }}</h6>
+          </div>
+        </v-flex>
+        <v-flex xs6 lg3>
+          <div class="date">
+            <h5 class="date__value">&lsaquo; date &rsaquo;</h5>
+            <h6 class="date__description">{{ $t('evaluations.forms.completed_at') }}</h6>
+          </div>
+        </v-flex>
+
+        <v-flex xs12>
+          <sections-composer
+            v-model="sections.models"
+            :editable="template.editable"
           />
-        </div>
-      </v-flex>
+        </v-flex>
+      </v-layout>
+    </v-form>
 
-      <v-flex xs12 lg6>
-        <div class="employee">
-          <h3 class="employee__fullname">
-            &lsaquo; {{ $t('templates.placeholders.name') }} &rsaquo;
-          </h3>
-          <h4 class="employee__position">
-            &lsaquo; {{ $t('templates.placeholders.position') }} &rsaquo;
-          </h4>
-        </div>
-      </v-flex>
-
-      <v-flex xs6 lg3>
-        <div class="date">
-          <h5 class="date__value">&lsaquo; date &rsaquo;</h5>
-          <h6 class="date__description">{{ $t('evaluations.forms.hired_at') }}</h6>
-        </div>
-      </v-flex>
-      <v-flex xs6 lg3>
-        <div class="date">
-          <h5 class="date__value">&lsaquo; date &rsaquo;</h5>
-          <h6 class="date__description">{{ $t('evaluations.forms.completed_at') }}</h6>
-        </div>
-      </v-flex>
-
-      <v-flex xs12>
-        <sections-composer
-          v-model="sections.models"
-          :editable="template.editable"
-        />
-      </v-flex>
-
+    <v-layout row>
       <v-flex xs12 lg6>
         <section-form
           v-if="template.editable"
