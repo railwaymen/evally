8d399f5c78c94e564e337c683d175e7aaf248edc
Szymon Chodzidlo <schodevio@gmail.com>
2020-02-04 16:38:06 +0100

    Prepare employees search query specs scenarios

---
+++ spec/queries/v2/employees_search_query_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe V2::EmployeesSearchQuery do
+  it 'finds employee with GIT equals 3 stars'
+
+  it 'finds employee with GIT greater than 2 stars'
+
+  it 'finds employee with GIT lower than 2 stars'
+
+  it 'finds employee who was a team leader'
+
+  it 'finds employee who was not a team leader'
+
+  it 'finds employee base on latest evaluation'
+
+  it 'uses equal sign when invalid operator'
+end
