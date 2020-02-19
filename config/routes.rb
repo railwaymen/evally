82a4de1e98635ef83ef5481fcb5a94189491db53
Szymon Chodzidlo <schodevio@gmail.com>
2020-02-19 12:07:49 +0100

    Add custom passwords controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /en|pl/ do
     scope :v2, defaults: { format: :json } do
       resource :session, controller: 'rails_jwt_auth/sessions', only: %i[create destroy]
-      resources :passwords, controller: 'rails_jwt_auth/passwords', only: %i[create update]
     end
 
     namespace :v2, defaults: { format: :json } do
       resources :invitations, controller: 'invitations', only: %i[create update]
+      resources :passwords, controller: 'passwords', only: %i[create update]
 
       resource :dashboard, controller: 'dashboard', only: :show
 
