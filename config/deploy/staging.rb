fd47ed56eeb280211addb3506eb50c6a3567dbd0
Szymon Chodzidlo <schodevio@gmail.com>
2020-02-19 12:17:02 +0100

    Update deploy configs

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,5 @@
 
 set :deploy_to, ENV['CAP_STAGING_DEPLOY_TO']
 server ENV['CAP_STAGING_SERVER'], user: ENV['CAP_STAGING_SSH_USER'], roles: %w[app db web]
-set :rvm_ruby_version, 'ruby-2.6.3'
+set :rvm_ruby_version, 'ruby-2.7.0'
 set :branch, :develop
