e5e39be57b3c9e9e7308bac7bfca6895d7098af1
Szymon Chodzidlo <schodevio@gmail.com>
2020-02-20 10:12:21 +0100

    Style auth mailer templates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.logger = ActiveSupport::Logger.new(STDOUT)
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 end
