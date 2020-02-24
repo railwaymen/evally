21f4659bd65c26fcb1b582d2ef7818a5c168c914
Szymon Chodzidlo <schodevio@gmail.com>
2020-02-24 08:46:04 +0100

    Fix migration

--- db/migrate/20200223105327_remove_password_digest_from_users.rb
+++ db/migrate/20200223105327_remove_password_digest_from_users.rb
@@ -1,5 +1,11 @@
 class RemovePasswordDigestFromUsers < ActiveRecord::Migration[5.2]
-  def change
+  def up
     remove_column :users, :password_digest, :string
+  end
+
+  def down
+    add_column :users, :password_digest, :string
+
+    User.all.each { |u| u.update(password: SecureRandom.hex ) }
   end
 end
