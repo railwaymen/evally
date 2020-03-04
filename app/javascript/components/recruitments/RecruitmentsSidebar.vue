72b44595a4da42b4aa27ed9b0c252c7ad644a1e1
Michal Szymanski <michal.szymanski@railwaymen.org>
2020-03-04 11:15:20 +0100

    Base sidebar for recruitment details

---
+++ app/javascript/components/recruitments/RecruitmentsSidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sidebar sidebar--boxed">
+    <div class="vcard">
+      <h3 class="vcard__fullname">{{ recruitment.fullname }}</h3>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { Recruitment } from '@models/recruitment'
+
+export default {
+  name: 'RecruitmentsSidebar',
+  props: {
+    recruitment: {
+      type: Recruitment,
+      required: true,
+      default: () => new Recruitment()
+    }
+  },
+  data() {
+    return {
+      panel: [0, 1]
+    }
+  }
+}
+</script>
--- app/javascript/views/recruitments/Show.vue
+++ app/javascript/views/recruitments/Show.vue
@@ -4,14 +4,29 @@
       <div class="panel__name">
         <h2>{{ $t('views.recruitments.show.title') }}</h2>
       </div>
+
+      <recruitments-sidebar
+        :recruitment="recruitment"
+      />
     </div>
   </section>
 </template>
 
 <script>
-
+import { mapGetters } from 'vuex'
+import { Recruitment } from '@models/recruitment'
+import RecruitmentsSidebar from '@components/recruitments/RecruitmentsSidebar'
 
 export default {
-  name: 'RecruitmentsShow'
+  name: 'RecruitmentsShow',
+  components: { RecruitmentsSidebar },
+  computed: {
+     ...mapGetters({
+      recruitment: 'RecruitmentsModule/recruitment'
+    })
+  },
+  created() {
+    this.$store.dispatch('RecruitmentsModule/show', this.$route.params.id)
+  },
 }
 </script>
