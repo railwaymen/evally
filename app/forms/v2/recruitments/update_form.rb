5a5e0f0c0b21075eb795a3e9ba9d2184c77f3d99
Szymon Chodzidlo <schodevio@gmail.com>
2020-03-04 12:43:48 +0100

    Add update action for recruitments controller

---
+++ app/forms/v2/recruitments/update_form.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module V2
+  module Recruitments
+    class UpdateForm
+      def initialize(recruitment, params:, user:)
+        @recruitment = recruitment
+
+        @recruitment.assign_attributes(params.fetch(:recruitment, {}))
+        candidate.assign_attributes(params.fetch(:candidate, {}))
+
+        @user = user
+      end
+
+      def save
+        validate_candidate_and_recruitment!
+
+        ActiveRecord::Base.transaction do
+          candidate.save!
+          @recruitment.save!
+
+          create_activity!
+        end
+      end
+
+      def recruitment
+        V2::Recruitments::BasicQuery.call.find_by(id: @recruitment.id)
+      end
+
+      private
+
+      def validate_candidate_and_recruitment!
+        return if [candidate.valid?, @recruitment.valid?].all?
+
+        raise ErrorResponderService.new(
+          :invalid_record, 422, candidate.errors.full_messages | @recruitment.errors.full_messages
+        )
+      end
+
+      def candidate
+        @candidate ||= @recruitment.candidate
+      end
+
+      def create_activity!
+        @user.activities.create!(
+          action: 'update',
+          activable: @recruitment,
+          activable_name: candidate.fullname
+        )
+      end
+    end
+  end
+end
--- spec/controllers/v2/recruitments_controller_spec.rb
+++ spec/controllers/v2/recruitments_controller_spec.rb
@@ -219,4 +219,139 @@ RSpec.describe V2::RecruitmentsController, type: :controller do
       end
     end
   end
+
+  describe '#update' do
+    context 'when unauthorized' do
+      it 'responds with 401' do
+        params = {
+          id: 1,
+          recruitment: {
+            first_name: 'Franek'
+          }
+        }
+
+        put :update, params: params
+        expect(response).to have_http_status 401
+      end
+    end
+
+    context 'when authorized' do
+      it 'updates candidate params' do
+        candidate = FactoryBot.create(:candidate)
+        recruitment = FactoryBot.create(:recruitment, candidate: candidate)
+
+        params = {
+          id: recruitment.id,
+          recruitment: {
+            first_name: 'Szczepan'
+          }
+        }
+
+        sign_in admin
+
+        expect do
+          put :update, params: params
+        end.to(change { candidate.reload.first_name }.to('Szczepan'))
+
+        expect(response).to have_http_status 200
+        expect(response.body).to be_json_eql recruitment_schema(recruitment.reload)
+      end
+
+      it 'updates requirement params' do
+        candidate = FactoryBot.create(:candidate)
+        recruitment = FactoryBot.create(:recruitment, candidate: candidate)
+
+        params = {
+          id: recruitment.id,
+          recruitment: {
+            group: 'Designers'
+          }
+        }
+
+        sign_in admin
+
+        expect do
+          put :update, params: params
+
+          recruitment.reload
+        end.to(change { recruitment.group }.to('Designers'))
+
+        expect(response).to have_http_status 200
+        expect(response.body).to be_json_eql recruitment_schema(recruitment)
+      end
+
+      it 'creates proper activity' do
+        candidate = FactoryBot.create(:candidate)
+        recruitment = FactoryBot.create(:recruitment, candidate: candidate)
+
+        params = {
+          id: recruitment.id,
+          recruitment: {
+            group: 'Designers'
+          }
+        }
+
+        sign_in admin
+
+        expect do
+          put :update, params: params
+        end.to(change { Activity.count }.by(1))
+      end
+
+      it 'responds with 422 if invalid candidate params' do
+        candidate = FactoryBot.create(:candidate)
+        recruitment = FactoryBot.create(:recruitment, candidate: candidate)
+
+        params = {
+          id: recruitment.id,
+          recruitment: {
+            first_name: ''
+          }
+        }
+
+        sign_in admin
+
+        expect do
+          put :update, params: params
+        end.not_to(change { candidate.reload.first_name })
+
+        expect(response).to have_http_status 422
+      end
+
+      it 'responds with 422 if invalid recruitment params' do
+        candidate = FactoryBot.create(:candidate)
+        recruitment = FactoryBot.create(:recruitment, candidate: candidate)
+
+        params = {
+          id: recruitment.id,
+          recruitment: {
+            group: ''
+          }
+        }
+
+        sign_in admin
+
+        expect do
+          put :update, params: params
+        end.not_to(change { recruitment.reload.group })
+
+        expect(response).to have_http_status 422
+      end
+
+      it 'responds with 404 if recruitment not found' do
+        params = {
+          id: 1,
+          recruitment: {
+            first_name: 'Szczepan',
+            group: 'Designers'
+          }
+        }
+
+        sign_in admin
+        put :update, params: params
+
+        expect(response).to have_http_status 404
+      end
+    end
+  end
 end
