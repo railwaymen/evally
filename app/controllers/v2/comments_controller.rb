3c58c60a87dfbbb4bb6557640e19765b4f292a07
Szymon Chodzidlo <schodevio@gmail.com>
2020-03-09 09:27:10 +0100

    Add comments controller specs

---
+++ app/controllers/v2/comments_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module V2
+  class CommentsController < ApplicationController
+    before_action :authenticate_user!
+
+    def create; end
+
+    def update; end
+
+    def destroy; end
+  end
+end
