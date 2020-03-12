b117943d7620f7c32d0bffa4f18164ec24e1dd69
Szymon Chodzidlo <schodevio@gmail.com>
2020-03-12 15:52:46 +0100

    Rewrite evaluation js model

--- app/javascript/models/evaluation.js
+++ app/javascript/models/evaluation.js
@@ -7,10 +7,10 @@ class Evaluation extends Model {
     return {
       id: null,
       employee_id: null,
-      employee_first_name: '',
-      employee_last_name: '',
-      employee_position: '',
-      employee_hired_on: null,
+      first_name: '',
+      last_name: '',
+      position: '',
+      hired_on: null,
       next_evaluation_on: null,
       completed_at: '',
       state: '',
@@ -23,12 +23,12 @@ class Evaluation extends Model {
     return moment(this.completed_at).format('MMM DD, YYYY')
   }
 
-  get employeeFullname() {
-    return [this.employee_first_name, this.employee_last_name].join(' ')
+  get Fullname() {
+    return [this.first_name, this.last_name].join(' ')
   }
 
-  get employeeHiredOn() {
-    return moment(this.employee_hired_on).format('MMMM YYYY')
+  get hiredOn() {
+    return moment(this.hired_on).format('MMMM YYYY')
   }
 
   get nextEvaluationOn() {
@@ -42,12 +42,12 @@ class Evaluation extends Model {
   }
 
   get draftPath() {
-    return { name: 'evaluation_path', params: { id: this.id } }
+    return { name: 'evaluation_draft_path', params: { id: this.id } }
   }
 
-  get path() {
+  get completedPath() {
     return {
-      name: 'evaluation_path',
+      name: 'evaluation_completed_path',
       params: {
         employeeId: this.employee_id,
         id: this.id
@@ -55,9 +55,9 @@ class Evaluation extends Model {
     }
   }
 
-  browsePath(employeeId) {
+  browserPath(employeeId) {
     return {
-      name: 'evaluation_browse_path',
+      name: 'evaluation_browser_path',
       params: {
         employeeId,
         id: this.id
@@ -67,9 +67,9 @@ class Evaluation extends Model {
 
   static get routes() {
     return {
-      draftsPath: '/v2/drafts',
-      draftPath: id => `/v2/drafts/${id}`,
-      evaluationPath: id => `/v2/evaluations/${id}`,
+      evaluationEmployablesPath: '/v2/evaluation_employables',
+      draftEvaluationEmployablePath: id => `/v2/evaluation_employables/${id}/draft`,
+      completedEvaluationEmployablePath: id => `/v2/evaluation_employables/${id}/completed`,
       browseEvaluationPath: (employeeId, id) => `/v2/browse/employees/${employeeId}/evaluations/${id}`
     }
   }
