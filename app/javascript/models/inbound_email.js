07782f7b1360b88024f05bb68d845dc0cfb02cb2
Szymon Chodzidlo <schodevio@gmail.com>
2020-07-27 22:50:08 +0200

    Add inbox view for recruitments

---
+++ app/javascript/models/inbound_email.js
@@ -0,0 +1,35 @@
+import { Model, List } from './base'
+import { currentTimezone } from '@utils/helpers'
+
+import moment from 'moment-timezone'
+
+class InboundEmail extends Model {
+  get defaults() {
+    return {
+      id: '',
+      message_id: '',
+      source: '',
+      subject: '',
+      created_at: '',
+      status: ''
+    }
+  }
+
+  get createdAt() {
+    return moment(this.created_at).tz(currentTimezone).format('DD MMM YYYY, HH:mm z')
+  }
+
+  static get routes() {
+    return {
+      inboundEmailsPath: '/v2/inbound_emails'
+    }
+  }
+}
+
+class InboundEmailsList extends List {
+  get model() {
+    return InboundEmail
+  }
+}
+
+export { InboundEmail, InboundEmailsList }
